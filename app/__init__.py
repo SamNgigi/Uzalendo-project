77f789c8dc9baa67b5f6bf289e46ad0b03786a97
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-02-13 13:20:25 +0300

    Configure __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_mail import Mail
+# from flask_simplemde import SimpleMDE
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+db = SQLAlchemy()
+mail = Mail()
+photos = UploadSet('photos', IMAGES)
+# simple = SimpleMDE()
+# Initializing app
+
+
+def create_app(config_name):
+
+    # Initializing app
+    app = Flask(__name__)
+
+    # Setting up Config
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extension
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    # Registering the main blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    # Registering the auth blueprint
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    # configure UploadSet
+    configure_uploads(app, photos)
+
+    return app
---
+++ config.py
@@ -0,0 +1,41 @@
+import os
+
+
+class Config:
+    """
+    Parent Config
+    """
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    # TODO: Remember to configure the psql uri
+
+
+class ProdConfig(Config):
+    """
+    Child Config with production configurations
+    """
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass
+
+
+class DevConfig(Config):
+    """
+    Child Config with development configurations
+    """
+    DEBUG = True
+
+
+class TestConfig(Config):
+    """
+    Child Config with test configurations
+    """
+    pass
+
+
+config_options = {
+    'dev': DevConfig,
+    'prod': ProdConfig,
+    'test': TestConfig
+}
