131bcba4d624a871dd880218d882a37e0c2b5eb2
MrazTevin <millatevin@gmail.com>
2018-02-15 11:17:53 +0300

    initial db configuration

--- config.py
+++ config.py
@@ -1,3 +1,24 @@
+
+import os
+
+
 class Config:
-    UPLOADED_PHOTOS_DEST = 'app/static/photos'
-    # UPLOADED_MEDIA_DEST = 'app/static/videos'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    Debug = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
+
+# class Config:
+#     UPLOADED_PHOTOS_DEST = 'app/static/photos'
+#     # UPLOADED_MEDIA_DEST = 'app/static/videos'
