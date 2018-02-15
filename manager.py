63400e23949f22d0a9ddbc9fac8547f34b6670c7
MrazTevin <millatevin@gmail.com>
2018-02-15 12:02:57 +0300

    model class imports

--- manager.py
+++ manager.py
@@ -1,6 +1,6 @@
 from app import create_app, db
 from flask_script import Manager, Server
-
+from app.models import Community, Reports, Comments
 
 # Creating app instance
 app = create_app('development')
@@ -12,7 +12,7 @@ manager.add_command('server', Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db)
+    return dict(app=app, db=db, Community=Community, Reports=Reports, Comments=Comments)
 
 
 if __name__ == '__main__':
