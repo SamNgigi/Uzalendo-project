332ee6a2397a95b4c17398cc98f9c677c77d9aa9
MrazTevin <millatevin@gmail.com>
2018-02-15 11:50:12 +0300

    made shell context

--- manager.py
+++ manager.py
@@ -5,7 +5,15 @@ from flask_script import Manager, Server
 # Creating app instance
 app = create_app('development')
 
+
 manager = Manager(app)
 manager.add_command('server', Server)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db)
+
+
 if __name__ == '__main__':
     manager.run()
