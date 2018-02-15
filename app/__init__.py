cc24d8bf9edd39350a9f9adc092a061055162e99
MrazTevin <millatevin@gmail.com>
2018-02-15 11:41:32 +0300

    initialized db

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from flask_uploads import UploadSet,configure_uploads,IMAGES
-
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_sqlalchemy import SQLAlchemy
 bootstrap = Bootstrap()
-photos = UploadSet('photos',IMAGES)
+db = SQLAlchemy()
+photos = UploadSet('photos', IMAGES)
 # media  = UploadSet('videos, default_dest=videos)
 
 
@@ -14,12 +15,13 @@ def create_app(config_name):
 
     # initialize flask extensions
     bootstrap.init_app(app)
+    db.init_app(app)
 
     # register blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     # configure UploadSet
-    configure_uploads(app,photos)
+    configure_uploads(app, photos)
 
     return app
