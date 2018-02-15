4842c25f6fc290443f613c090ff1f6f1ba224b35
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-02-15 19:19:05 +0300

    Configured mail in config

--- config.py
+++ config.py
@@ -10,6 +10,13 @@ class Config:
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sam:Sam@localhost/mzalendo'
 
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
 
 class ProdConfig(Config):
     """
